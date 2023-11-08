c0f09cc566c8d960509757630595cece68f8c314
sakib ul alam <sakibalambulbul1@gmail.com>
2023-11-08 18:54:40 +0600

    added a comment section in blogdetails and send those data to backend

--- src/Pages/Blogdetails/Blogdetails.jsx
+++ src/Pages/Blogdetails/Blogdetails.jsx
@@ -1,12 +1,37 @@
+import { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Authprovider/Authprovider";
+import axios from "axios";
 
 
 
 const Blogdetails = () => {
 
+    const {user} = useContext(AuthContext)
+    console.log(user)
+    const username = user?.displayName;
+    const userphoto = user?.photoURL;
+    console.log(username,userphoto)
+
+
     const alldata = useLoaderData();
     console.log(alldata)
-    const {title,image,shortdes,category,longdes} = alldata;
+    const {title,image,shortdes,longdes} = alldata;
+
+    const handlecomment = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const comment = form.comment.value;
+        console.log(comment)
+        const allinfo = {username,userphoto,comment}
+        
+
+        axios.post('http://localhost:5000/comment',allinfo)
+        .then(res=>{
+            console.log(res.data)
+        })
+       
+    }
 
    
    
@@ -18,6 +43,13 @@ const Blogdetails = () => {
                 <h1 className="font-extrabold text-2xl">{title}</h1>
                 <p className="font-medium text-base mb-6">{shortdes}</p>
                 <p className="font-medium text-base mb-6">{longdes}</p>
+            </div>
+            <div>
+                <h1 className="font-bold text-xl">Post a Comment</h1>
+                <form onSubmit={handlecomment}>
+                <textarea placeholder="Your comment" name="comment" className="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
+               <br /> <button className="btn btn-outline">post</button>
+                </form>
             </div>
         </div>
     );
