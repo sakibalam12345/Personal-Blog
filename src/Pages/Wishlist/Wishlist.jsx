00d63be8687f03f3a644a9bb79e41ad23e03d6fa
sakib ul alam <sakibalambulbul1@gmail.com>
2023-11-08 12:12:45 +0600

    made a wishlist route and loaded unique data which user selected as wishlist

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -1,9 +1,35 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import Wishlistcardshow from "./Wishlistcardshow";
 
 
 const Wishlist = () => {
+
+    const [wishlisted,setwishlisted] = useState([])
+
+    useEffect(()=>{
+        axios.get('http://localhost:5000/wishlist')
+        .then(res=>{
+            console.log(res.data)
+            setwishlisted(res.data)
+        })
+    },[])
+
+    const handledelete = id =>{
+        console.log(id)
+    }
     return (
         <div className="max-w-7xl mx-auto">
             <h1 className="font-bold text-3xl text-center mb-10">Your Selected wishlist</h1>
+            <div className="grid grid-cols-1">
+                {
+                    wishlisted.map(item=> <Wishlistcardshow key={item._id} 
+                        handledelete={handledelete}
+                        
+                        item={item}></Wishlistcardshow>)
+                }
+            </div>
         </div>
     );
 };
--- src/Pages/Wishlist/Wishlistcardshow.jsx
+++ src/Pages/Wishlist/Wishlistcardshow.jsx
@@ -1,10 +1,21 @@
 
 
-const Wishlistcardshow = () => {
+const Wishlistcardshow = ({item,handledelete}) => {
+
+    const {title,image,shortdes,category,longdes,_id} = item;
     return (
-        <div>
-            
+        <div className="max-w-7xl mx-auto">
+        <div className="card w-[900px] h-[600px] lg:card-side bg-base-100 shadow-xl mb-7">
+        <figure><img className="h-full w-[1250px]" src={image} alt="Album"/></figure>
+        <div className="card-body">
+          <h2 className="card-title">{title}</h2>
+          <p>{shortdes}</p>
+          <div className="card-actions justify-end">
+            <button onClick={()=>handledelete(_id)} className="btn btn-outline">Remove</button>
+          </div>
         </div>
+      </div>
+      </div>
     );
 };
 
