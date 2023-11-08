3328e8ec92d7ddf453df31f637e818e85e1ff490
sakib ul alam <sakibalambulbul1@gmail.com>
2023-11-08 12:49:00 +0600

    added a delete button and req axios delete option and made  remove unique data from database and also from client side

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -16,17 +16,16 @@ const Wishlist = () => {
         })
     },[])
 
-    const handledelete = id =>{
-        console.log(id)
-    }
+   
     return (
         <div className="max-w-7xl mx-auto">
             <h1 className="font-bold text-3xl text-center mb-10">Your Selected wishlist</h1>
             <div className="grid grid-cols-1">
                 {
                     wishlisted.map(item=> <Wishlistcardshow key={item._id} 
-                        handledelete={handledelete}
-                        
+                      
+                        wishlisted={wishlisted}
+                        setwishlisted={setwishlisted}
                         item={item}></Wishlistcardshow>)
                 }
             </div>
--- src/Pages/Wishlist/Wishlistcardshow.jsx
+++ src/Pages/Wishlist/Wishlistcardshow.jsx
@@ -1,8 +1,40 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
 
 
-const Wishlistcardshow = ({item,handledelete}) => {
+const Wishlistcardshow = ({item,wishlisted,setwishlisted}) => {
 
     const {title,image,shortdes,category,longdes,_id} = item;
+
+    const handledelete = id =>{
+        // console.log(id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                axios.delete(`http://localhost:5000/wishlist/${id}`)
+                .then(res=>{
+                    console.log(res.data)
+                    if(res.data.deletedCount > 0 ){
+                        toast('successfully deleted')
+                    }
+
+                })
+                const remaining = wishlisted.filter(list=> list._id !== id)
+                setwishlisted(remaining)
+            }
+           
+
+          });
+       
+    }
     return (
         <div className="max-w-7xl mx-auto">
         <div className="card w-[900px] h-[600px] lg:card-side bg-base-100 shadow-xl mb-7">
